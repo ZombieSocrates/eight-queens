7ba99763d932429bbc1fecb1c96025251480aa1c
Ross Weijer <ross@datascopeanalytics.com>
2019-05-26 15:20:19 -0500

    I think I solved diagonal motion

--- chess_board.py
+++ chess_board.py
@@ -1,6 +1,9 @@
 import random
 import ipdb
 
+from pprint import pprint
+from collections import Counter
+
 def make_chess_board(dim = 8, queen_loc_seed = None):
     '''Sets up a chessboard by randomly placing a single 
     queen in each row of the board
@@ -17,7 +20,7 @@ def make_chess_board(dim = 8, queen_loc_seed = None):
 
 def display_chess_board(chess_board):
     '''Prints all the rows in a given chessboard, with 1's
-    representing where queens are'''
+    representing where queens are. Could also just pprint this'''
     for chess_row in chess_board:
         print(chess_row)
 
@@ -34,28 +37,111 @@ def search_across_row(chess_row, from_column = 0):
         return None
 
 
-def locate_the_queens(chess_board):
+def locate_the_queens(chess_board, verbose = True):
     '''Calls search_across_row for every row in the chess_board, continuing 
     only when no more queens are found in the row. Will print the location 
-    of any queen it finds
-
-    TODO: Might be necessary to return the positions of each queen?
+    of any queen it finds. Also returns the positions of each queen in a
+    dictionary of format int : location tuple
     '''
+    queen_locations = {}
+    queen_count = 0
     for i, chess_row in enumerate(chess_board):
         j = search_across_row(chess_row)
         if j is None:
-            print(f"No queens in row {i}")
+            if verbose:
+                print(f"No queens in row {i}")
             continue
         else:
             while j is not None:
-                print(f"Queen found at row {i}, column {j}")
+                if verbose:
+                    print(f"Queen found at row {i}, column {j}")
+                queen_locations[queen_count] = (i , j)
+                queen_count += 1 
                 j = search_across_row(chess_row, from_column = j + 1)
+    return queen_locations
 
 
-        
+def count_row_conflicts(queen_locations):
+    row_counts = Counter([q[0] for q in queen_locations.values()])
+    return sum([v - 1 for v in row_counts.values()])
+
+
+def count_col_conflicts(queen_locations):
+    col_counts = Counter([q[1] for q in queen_locations.values()])
+    return sum([v - 1 for v in col_counts.values()])
+
+
+def move_up_and_left(queen_location):
+    new_location = queen_location[0] - 1, queen_location[1] - 1
+    if (new_location[0] >= 0) and (new_location[1] >= 0):
+        return new_location
+    return None
+
+
+def move_up_and_right(queen_location):
+    '''get rid of hard-coded n-queens minus 1
+    '''
+    new_location = queen_location[0] - 1, queen_location[1] + 1
+    if (new_location[0] >= 0) and (new_location[1] <= 7):
+        return new_location
+    return None
+
+
+def move_down_and_right(queen_location):
+    '''get rid of hard-coded n-queens minus 1
+    '''
+    new_location = queen_location[0] + 1, queen_location[1] + 1
+    if (new_location[0] <= 7) and (new_location[1] <= 7):
+        return new_location
+    return None
+
+
+def move_down_and_left(queen_location):
+    '''get rid of hard-coded n-queens minus 1
+    '''
+    new_location = queen_location[0] + 1, queen_location[1] - 1
+    if (new_location[0] <= 7) and (new_location[1] >= 0):
+        return new_location
+    return None
+
+
+def get_diagonals(q_position):
+    '''This is probably going to be ugly
+    '''
+    diags = []
+    diag_checker = {"up_left":move_up_and_left, 
+        "up_right":move_up_and_right,
+        "down_right":move_down_and_right,
+        "down_left":move_down_and_left,
+        }
+    for d in diag_checker.keys():
+        print(f"MOVING {d.replace('_',' ')}")
+        new_pos = diag_checker[d](q_position)
+        if new_pos is None:
+            continue
+        else:
+            while new_pos is not None:
+                diags.append(new_pos)
+                new_pos = diag_checker[d](new_pos)
+    return diags
+
+
+def check_if_solved(queen_locations):
+    '''TODO: implement diagonal checks
+    '''
+    row_conf = count_row_conflicts(queen_locations)
+    col_conf = count_col_conflicts(queen_locations)
+    if (row_conf + col_conf) == 0:
+        print("WE SOLVED IT, BRO!!!")
+    else:
+        print("Not solved...")
+        print(f"We still have {row_conf} row conflicts")
+        print(f"and {col_conf} column conflicts")
+
 
 if __name__ == "__main__":
     dat_board = make_chess_board(queen_loc_seed = 42)
     display_chess_board(dat_board)
-    locate_the_queens(dat_board)
+    q_locs = locate_the_queens(dat_board)
+    check_if_solved(q_locs)
     ipdb.set_trace()
