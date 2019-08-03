b138633b354847d3969d24589b66a285281e4927
Ross Weijer <ross@datascopeanalytics.com>
2019-08-02 18:22:15 -0700

    chessBoard class finished and functional in demo.py script

--- chess_board.py
+++ chess_board.py
@@ -1,6 +1,5 @@
-import random
-import pprint
 import ipdb
+import random
 
 from collections import Counter
 
@@ -74,24 +73,136 @@ class chessBoard(object):
             print(f"Queen {q + 1} is at row {r + 1}, column {c + 1}")
 
 
-    def queens_to_integer(self):
+    def get_state_string(self):
         '''Returns a representation of the chess board's current queen 
-        positions as a (dim*2)-digit integer. Every two digits of this 
-        integer is the row, column location of a queen. 
+        positions as a (dim*2)-digit string. Every two characters is the row, 
+        column location of a queen. 
 
-        TODO: Is it really worth some imagined performance gain to use an 
-        integer? Storing it as a string makes it easier for us to index and 
+        Storing it as a string makes it easier for us to index and 
         validate ... 
         '''
         loc_strs = [f"{v[0] + 1}{v[1] + 1}" for v in self.q_locs.values()]
-        return int("".join(loc_strs))
+        return "".join(loc_strs)
 
 
-    #TODO: check direction methods need to be in here
+    def check_up_and_left(self, coord):
+        '''Generate a valid coordinate one unit up and left from coord.
+        Would be a private method if python did that ...
+        '''
+        new_coord = coord[0] - 1, coord[1] - 1
+        if (new_coord[0] >= 0) and (new_coord[1] >= 0):
+            return new_coord
+        return None
 
-    #TODO: Maybe the conflict measurement ones need to be here too?
+
+    def check_up_and_right(self, coord):
+        '''Generate a valid coordinate one unit up and right from coord.
+        Would be a private method if python did that ...
+        '''
+        bound = self.dim - 1
+        new_coord = coord[0] - 1, coord[1] + 1
+        if (new_coord[0] >= 0) and (new_coord[1] <= bound):
+            return new_coord
+        return None
 
 
+    def check_down_and_right(self, coord):
+        '''Generate a valid coordinate one unit down and right from coord.
+        Would be a private method if python did that ...
+        '''
+        bound = self.dim - 1
+        new_coord = coord[0] + 1, coord[1] + 1
+        if (new_coord[0] <= bound) and (new_coord[1] <= bound):
+            return new_coord
+        return None
+
+
+    def check_down_and_left(self, coord):
+        '''Generate a valid coordinate one unit down and left from coord.
+        Would be a private method if python did that ...
+        '''
+        bound = self.dim - 1
+        new_coord = coord[0] + 1, coord[1] - 1
+        if (new_coord[0] <= bound) and (new_coord[1] >= 0):
+            return new_coord
+        return None
+
+
+    def get_diagonals(self, coord):
+        '''Calculates all the relative diagonal positions from the given 
+        coordinate, mainly for the purpose of saying "Is there a queen in any 
+        of these diagonal positions?"
+        '''
+        diags = []
+        diag_checker = {"up_left":self.check_up_and_left, 
+            "up_right":self.check_up_and_right,
+            "down_right":self.check_down_and_right,
+            "down_left":self.check_down_and_left
+            }
+        for d in diag_checker.keys():
+            new_pos = diag_checker[d](coord)
+            if new_pos is None:
+                continue
+            else:
+                while new_pos is not None:
+                    diags.append(new_pos)
+                    new_pos = diag_checker[d](new_pos)
+        return diags
+
+    
+    def count_orthogonal_conflicts_by_queen(self):
+        '''Returns a dictionary keyed by queen_index (0th queen, 1st queen) 
+        with values as the number of conflicts that queen is creating in 
+        orthogonal directions (rows or columns).
+
+        TODO: Maybe some day disambiguating between row conflicts and column 
+        conflicts will be helpful?
+        '''
+        orth_conf_dict = {}
+        for k in self.q_locs.keys():
+            orth_conf_dict[k] = 0
+            for d in [0,1]:
+                curr_d = self.q_locs[k][d]
+                d_count = [1 for v in self.q_locs.values() if v[d] == curr_d]
+                orth_conf_dict[k] += sum(d_count) - 1
+        return orth_conf_dict
+
+
+    def count_diagonal_conflicts_by_queen(self):
+        '''Returns a dictionary keyed by queen_index (0th queen, 1st queen) 
+        with values as the number of conflicts that queen is creating in 
+        diagonal directions.
+        '''
+        diag_conf_dict = {}
+        for k in self.q_locs.keys():
+            this_qn = self.q_locs[k]
+            diag_sqrs = self.get_diagonals(this_qn)
+            othr_qns = [q for q in self.q_locs.values() if q != this_qn]
+            diag_conf_dict[k] = len(set(diag_sqrs).intersection(set(othr_qns)))
+        return diag_conf_dict
+
+
+    def combine_conflict_counts(self, conflict_dicts):
+        '''Helper functioned designed to sum the outputs of orthogonal and 
+        diagonal conflict dicts together.
+        '''
+        all_confs = None
+        for conf in conflict_dicts:
+            if all_confs is None:
+                all_confs = conf.copy()
+                continue
+            for k in conf.keys():
+                all_confs[k] += conf[k]
+        return all_confs
+
+
+    def count_conflicts_by_queen(self):
+        '''Combine the results of the above three methods, breh. Then deletes any 
+        queen key that does not have any conflicts.'''
+        conf_dicts = [self.count_orthogonal_conflicts_by_queen(),
+            self.count_diagonal_conflicts_by_queen()]
+        conf_smash = self.combine_conflict_counts(conf_dicts)
+        return {k:v for k,v in conf_smash.items() if v!= 0}
 
 
 if __name__ == "__main__":
@@ -99,5 +210,5 @@ if __name__ == "__main__":
     qB = chessBoard(dimension = 8, queen_seed = 50)
     qB.display()
     qB.show_queen_locations()
-    print(qB.queens_to_integer())
+    print(qB.get_state_string())
     ipdb.set_trace()
